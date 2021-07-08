7dd1a812e45ab128d08858b1da03a0ccd1906c43
unknown <lucasmartins_eu@hotmail.com>
2021-07-08 18:28:11 -0300

    Primeiro módulo do curso de desenvolvilmento de APIs com ASP.NET Core

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,86 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+            _option = option.Value;
+        }
+
+        //api/projects?query=string
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        //api/projects/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        //api/projects
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        //api/projects/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        //api/projects/3
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //buscar se id existe, se não, retornar NotFound()
+
+            return NoContent();
+        }
+
+        //api/projects/1/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment ([FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        //api/projects/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        //api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
